d680b2c3fa7e5a36dad4c8312558935020cae57b
Kappa56799 <kacper200657@hotmail.com>
2023-05-15 14:16:28 +0100

    first working commit xD

--- downloader.py
+++ downloader.py
@@ -1,5 +1,7 @@
 from pytube import YouTube
 import customtkinter as ctk
+from threading import Thread
+from moviepy.editor import *
 
 ctk.set_appearance_mode("system")
 ctk.set_default_color_theme("dark-blue")
@@ -10,42 +12,50 @@ root.geometry("500x500")
 radiovar = ctk.IntVar()
 
 def download():
+    progressbar.set(0)
     link = entry1.get()
     yt = YouTube(link)
 
-    if radiovar.get() == 1:
-        video = yt.streams.get_highest_resolution()
-        video.download()
-        print("Video Downloaded")
-
-    elif radiovar.get() == 2:
-        audio = yt.streams.filter(only_audio=True).first()
-        audio.download()
-        print("Audio Downloaded")
+    video = yt.streams.get_highest_resolution()
+    progressbar.set(50)
+    video.download()
+    print("Video Downloaded")
+    progressbar.set(100)
 
 
+def downloadFile():
+    Thread(target=download).start()
 
-def radiobutton_event():
-    print("radiobutton toggled, current value: " + radiovar.get())
+def UploadAction(event=None):
+    filename = ctk.filedialog.askopenfilename()
+    print('Selected:', filename)
 
+    video = VideoFileClip(filename)
+    video.audio.write_audiofile(filename[:-4] + ".mp3")
     
 
 frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = ctk.CTkLabel(master=frame, text="Youtube Downloader", font=("Arial", 20))
+label = ctk.CTkLabel(master=frame, text="Youtube Downloader", font=("Arial", 40))
 label.pack(pady=12, padx=10)
 
-entry1 = ctk.CTkEntry(master=frame, placeholder_text="Enter the link")
+entry1 = ctk.CTkEntry(master=frame, width=350, height=30, placeholder_text="Enter the link")
 entry1.pack(pady=12, padx=10)
 
-radio1 = ctk.CTkRadioButton(master=frame, text="Video", variable=radiovar, value="1")
-radio1.pack(pady=12, padx=10)
-
-radio2 = ctk.CTkRadioButton(master=frame, text="Audio", variable=radiovar, value="2")
-radio2.pack(pady=12, padx=10)
-
-button = ctk.CTkButton(master=frame, text="Download", command=download)
+button = ctk.CTkButton(master=frame, width=150, text="Download", command=downloadFile)
 button.pack(pady=12, padx=10)
+
+progressbar = ctk.CTkProgressBar(master=frame,mode="determinate", width=400)
+progressbar.pack(pady=20, padx=10)
+progressbar.set(0)
+
+label = ctk.CTkLabel(master=frame, text="Convert A file from MP4 to MP3", font=("Arial", 40))
+label.pack(pady=12, padx=10)
+
+
+button = ctk.CTkButton(master=frame, width=150, text="Select A File and Convert", command=UploadAction)
+button.pack(pady=12, padx=10)
+
 
 root.mainloop()
